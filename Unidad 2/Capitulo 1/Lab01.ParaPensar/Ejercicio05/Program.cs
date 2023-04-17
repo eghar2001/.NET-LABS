0188f4880eb755b4201beb0d2045733802027fe0
eghar2001 <nahucarc2@gmail.com>
2023-04-17 12:23:51 -0300

    Lab01- Termine hasta el ejercicio 5 de ParaPensar

---
+++ Unidad 2/Capitulo 1/Lab01.ParaPensar/Ejercicio05/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ejercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthToInt(string month)
+            {
+                string mesClave = month.ToLower();
+                Dictionary<string, int> parMesEntero = new Dictionary<string, int>() {
+                    { "enero",1},
+                    {"febrero",2},  
+                    {"marzo",3},
+                    {"abril",4 },
+                    {"mayo",5 },
+                    {"junio",6 },
+                    {"julio",7 },
+                    {"agosto",8 },
+                    {"septiembre",9 },
+                    {"octubre",10 },
+                    {"noviembre",11 },
+                    {"diciembre",12 }
+                };
+                if (parMesEntero.ContainsKey(mesClave))
+                {
+                    return parMesEntero[mesClave];
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            Console.Write("Ingrese el mes en texto: ");
+            string? mesString = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"El mes {mesString.ToLower()} tiene el numero: {monthToInt(mesString)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Unidad 2/Capitulo 1/Lab01/Desicion/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Desicion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+
+            /*
+            Console.Write("Ingrese un texto: ");
+            string? inputTexto = Console.ReadLine();
+            if (!String.IsNullOrEmpty(inputTexto))
+
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("Ingrese una opcion:");
+                Console.WriteLine("1-Pasar texto a MAYUSCULA");
+                Console.WriteLine("2-Pasar texto a minuscula");
+                Console.WriteLine("3- Mostrar largo del texto");
+               
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine();
+                if(opcion.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine($"El text en mayuscula es: {inputTexto.ToUpper()}");
+                }
+                else if(opcion.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine($"El texto en minuscula es: {inputTexto.ToLower()}");
+                }
+                else if( opcion.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine($"El largo de la cadena es: {inputTexto.Length}");
+                }
+                else
+                {
+                    Console.WriteLine("No ingresó una opción válida");
+                }
+                Console.ReadKey();
+            */
+
+
+            //Ejercicio 2
+            Console.Write("Ingrese un texto: ");
+            string? inputTexto = Console.ReadLine();
+            Console.WriteLine();
+            if (!String.IsNullOrEmpty(inputTexto))
+
+            {
+                
+
+                Console.WriteLine("Ingrese una opcion:");
+                Console.WriteLine("1-Pasar texto a MAYUSCULA");
+                Console.WriteLine("2-Pasar texto a minuscula");
+                Console.WriteLine("3- Mostrar largo del texto");
+
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine();
+                switch (opcion.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine($"El texto en mayuscula es: {inputTexto.ToUpper()}");
+                        break;
+
+                    case ConsoleKey.D2:
+                        Console.WriteLine($"El texto en minuscula es: {inputTexto.ToLower()}");
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine($"El largo del texto es: {inputTexto.Length}");
+                        break;
+                    default:
+                        Console.WriteLine("No ingresó una opcion correcta");
+                        break;
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Ponga un texto");
+            }
+            
+        }
+    }
+}
