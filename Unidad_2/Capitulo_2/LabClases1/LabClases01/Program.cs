76835e4dd1c7d916f775efb3ca77dfd20873458e
eghar2001 <nahucarc2@gmail.com>
2023-04-25 10:28:14 -0300

    Comienzo Unidad2-capitulo2-lab1: Orientacion a objetos

---
+++ Unidad_2/Capitulo_2/LabClases1/LabClases01/Program.cs
@@ -0,0 +1,29 @@
+﻿using Clases;
+
+namespace LabClases01
+{
+    internal class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            A instanciaA = new A();
+            B instanciaB = new B();
+
+            Console.WriteLine("Metodos de A:\n");
+            //Metodos de A
+            instanciaA.MostrarNombre();
+            instanciaA.M1();
+            instanciaA.M2();
+            instanciaA.M3();
+
+            Console.WriteLine("\n\nMetodos de B:\n");
+            //Metodos de B
+            instanciaB.MostrarNombre();
+            instanciaB.M1();
+            instanciaB.M2();
+            instanciaB.M3();
+            instanciaB.M4();
+        }
+    }
+}
